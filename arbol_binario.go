a2f09292ca9c60040f3443322c1ddfffc1a7a92b
Revanitspa <revanjz6@gmail.com>
2017-01-05 01:39:05 -0600

    Add files via upload

--- arbol_binario.go
+++ arbol_binario.go
@@ -1,5 +1,6 @@
 /*
-Alpha version 1.2
+Alpha version 2.1
+RevanJZ
 */
 
 package main
@@ -9,19 +10,42 @@ type arbol struct {
 	pinpollo int
 	izq,der *arbol
 }
-func crearpinpollo(x int) *arbol {
+func Crearpinpollo(x int) *arbol {
 mao := new (arbol)
 	mao.pinpollo = x
 	mao.izq = nil
 	mao.der = nil
 	return mao
 }
-func insertarpinpollo(maokai *arbol, x int) {
+func Insertarpinpollo(maokai *arbol, x int) *arbol  {
+
+	if maokai == nil {
+		
+		return Crearpinpollo(x)
+		
+
+		
+	}else if x < maokai.pinpollo {
+		maokai.izq=Insertarpinpollo(maokai.izq, x)
+		return maokai
+		
+	}else{
+		maokai.der=Insertarpinpollo(maokai.der, x)
+		return maokai
+	}
 	
 }
 func main() {
-mao := new (arbol)
-mao= crearpinpollo(12)
+var mao *arbol
+ mao =Insertarpinpollo(mao,12)
  fmt.Println(mao.pinpollo)
- fmt.Println(mao.izq)	
+ fmt.Println(mao.izq)
+ fmt.Println(mao.der)
+ mao =Insertarpinpollo(mao,1)
+ fmt.Println(mao.pinpollo)
+ fmt.Println(mao.izq)
+ fmt.Println(mao.der)
+
+ 
+
 }
